f480968199271d15125b823143aab9c897f8d0a7
Nikhil <4531201+nikhil1415926@users.noreply.github.com>
2019-01-25 12:01:49 -0500

    removed ethpm package

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "zeppelin/contracts/math/SafeMath.sol";
+import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 /** @title Marketplace contract. */
