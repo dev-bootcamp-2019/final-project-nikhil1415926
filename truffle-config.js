cf85871fea962165130f812e811de5201ffe1962
Nikhil <4531201+nikhil1415926@users.noreply.github.com>
2019-01-27 13:08:46 -0500

    changes

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "glow impact title traffic hood velvet praise share muscle alley bless brother";
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -6,6 +8,13 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: "*"
-    }
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/91928d8fc6e64ba78602f274b01d9d85")
+      },
+      network_id: 3
+    }   
+
   }
 };
